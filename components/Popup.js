20ba824088aa472bc827247f8378d7274933f8fd
eduenez33 <duenezedwin33@gmail.com>
2025-05-24 12:18:55 -0600

    add or remove event listener
    when the modal opens, add the event listener on keyup that calls the method to close when the escape key is pressed and vice-versa on the close method

--- components/Popup.js
+++ components/Popup.js
@@ -12,10 +12,12 @@ class Popup {
 
   open() {
     this._popupElement.classList.add("popup_visible");
+    document.addEventListener("keyup", this._handleEscapeClose);
   }
 
   close() {
     this._popupElement.classList.remove("popup_visible");
+    document.removeEventListener("keyup", this._handleEscapeClose);
   }
 
   setEventListeners() {
